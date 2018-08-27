203aff9ac76ac11848ee5eb2d81a5fdba2699cab
Justin Franz <JFranz@uscloud.com>
2018-08-27 15:41:44 -0500

    more updates

--- ExpressApp1/handler.js
+++ ExpressApp1/handler.js
@@ -1,53 +1,26 @@
 'use strict';
-var AWS = require("aws-sdk");
 var fs = require('fs');
+
+var AWS = require('aws-sdk'),
+    documentClient = new AWS.DynamoDB.DocumentClient();
 
 AWS.config.update({
     region: "us-east-2",
 });
-
-    var docClient = new AWS.DynamoDB.DocumentClient();
-
-module.exports.hello = async (event, context) => {
-    var hello = "Hello Wrold";
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        value: hello,
-    //  input: event,
-    }),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-
-
-module.exports.database = async (event, context) => {
-    var hello = "Database ready to go";
-
-    var table = "Tracker";
-    var params = {
-        TableName: table
-    };
-    console.log(params);
-    docClient.scan(params, function (err, data) {
-        console.log("inside");
-        if (err) {
-            callback(err, null);
-        } else {
-            callback(null, data.Items);
-            return {
-                statusCode: 200,
-                body: JSON.stringify({
-                    message: 'database pulled'
-                }),
-            };
+exports.database = function (event, context,callback){
+    'use strict';
+        var params = {
+            TableName: 'Mileage'
+        };
+        documentClient.scan(params, function (err, data) {
+            if (err) {
+                callback(err, null);
+            } else {
+                callback(null, data);
+                console.log(data);
+            }
         }
-    });
-
-    
+        );
 
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
     // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
